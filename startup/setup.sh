909b35e6aa4cf02fa3ac1de0e2ee79ae88ee54d4
greenwind89 <minh.truonganh7@gmail.com>
2013-07-14 01:56:09 +0700

    Ok to commit

--- startup/setup.sh
+++ startup/setup.sh
@@ -34,10 +34,10 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+
+# ln -sb ./.screenrc .
+# ln -sb ./.bash_profile .
+# ln -sb ./.bashrc .
+# ln -sb ./.bashrc_custom .
+# ln -sf ./.emacs.d .
 
