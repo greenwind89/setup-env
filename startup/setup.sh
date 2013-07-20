bb1da151be118f0b8c87731420c0ee0f0a08d6e9
Truong Anh Minh <minh.truonganh7@gmail.com>
2013-07-20 13:58:39 +0700

    Add installing npm

--- startup/setup.sh
+++ startup/setup.sh
@@ -6,7 +6,11 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git-core
 
+# install nodejs
 sudo apt-get install nodejs
+
+# node package modules 
+sudo apt-get install npm
 
 # multi screen management
 sudo apt-get install tmux
@@ -15,7 +19,7 @@ sudo apt-get install tmux
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
+# Load nvm - node version management and install latest production node
 source $HOME/.nvm/nvm.sh
 nvm install v0.10.12
 nvm use v0.10.12
