83224850e1e5053e5b459a90a424aac68062b6f0
Truong Anh Minh <minh.truonganh7@gmail.com>
2013-07-20 13:43:10 +0700

    Add installing tmux

--- startup/setup.sh
+++ startup/setup.sh
@@ -8,6 +8,9 @@ sudo apt-get install -y git-core
 
 sudo apt-get install nodejs
 
+# multi screen management
+sudo apt-get install tmux
+
 #install heroku
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
